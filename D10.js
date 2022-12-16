9881443b2131b0ad7b1adde51d1b7eafc348b814
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-16 17:04:21 +0100

    Update D10.js

--- D10.js
+++ D10.js
@@ -346,15 +346,16 @@ function countMovies(array) {
     return array.length;
 }
 
-console.log(countMovies(movies));
+//console.log(countMovies(movies));
 
 /* ESERCIZIO 14
   Scrivi una funzione chiamata "onlyTheYears" che crea un array con solamente gli anni di uscita dei film contenuti nell'array "movies" fornito.
 */
 
 function onlyTheYears(array) {
-    return array.
+    return array.map((film) => film.Year);
 }
+console.log(onlyTheYears(movies));
 /* ESERCIZIO 15
       Scrivi una funzione chiamata "onlyInLastMillennium" che ritorna solamente i film prodotto nel millennio scorso contenuti nell'array "movies" fornito.
     */
